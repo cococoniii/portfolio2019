c4f0f54fd74003c5e83e8d2735729a578ef49db7
Fakear Huang <lamor4971@gmail.com>
2019-04-26 11:38:19 +0800

    Uhua

--- js/base.js
+++ js/base.js
@@ -1,1 +1,33 @@
 document.querySelector("#year").innerHTML = new Date().getFullYear();
+var app1 = new Vue({
+  el:'#app1',
+  data:{
+    navList: [
+      {
+        text: 'PORTFOLIO',
+        url: 'index.html'
+      },
+      {
+        text: 'MESSAGE',
+        url: 'message.html'
+      },
+      {
+        text: 'LOCKITIN',
+        url: 'lockitin.html'
+      },
+      {
+        text: 'CAMPING',
+        url: 'camping.html'
+      },
+      {
+        text: 'VISUAL',
+        url: 'visual.html'
+      }
+    ]
+  },
+  computed:{
+    currentPage: function(){
+      return location.pathname.replace('/','');
+    }
+  }
+})
